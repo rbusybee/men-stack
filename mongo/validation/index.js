e258dbd7f202865537edf0a97b2b8e5809e0e72c
rbusybee <romiococ@gmail.com>
2020-11-14 13:48:41 +0530

    Added 'required' Validation Check on Schema

--- mongo/validation/index.js
+++ mongo/validation/index.js
@@ -7,13 +7,46 @@ mongoose.connect('mongodb://localhost/playground')
 
 
 // Schema
-// const courseSchema = new mongoose.Schema({
-//     name: String,
-//     author: String,
-//     tags: [String],
-//     date: { type: Date, default: Date.now },
-//     isPublished: Boolean
-// });
+const courseSchema = new mongoose.Schema({
+    name: {type: String, required: true },
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+    price: Number
+});
 
-// // Model
-// const Course = mongoose.model('Courses', courseSchema, 'courses');
+// Model
+const Course = mongoose.model('Courses', courseSchema, 'courses');
+
+// Saving Data
+async function createCourse() {
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Romio',
+        tags: ['angular', 'frontend'],
+        isPublished: true,
+        price: 15
+    });
+    
+    try{
+        // Using Callback to validate and continue
+        // await course.validate((err) => {
+        //     if(err) {
+        //         console.log('Problem Occured', err);
+        //     } else {
+        //         console.log('No Problem');
+        //     }
+        // });
+
+        // Using Implicit validation checking during Save()
+        const result = await course.save();
+        console.log(result);
+    }
+    catch(ex) {
+        console.log(ex.message);
+    }
+}
+
+// Calling
+createCourse();
