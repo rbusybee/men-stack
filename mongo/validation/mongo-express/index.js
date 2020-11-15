fb4bf4d789c90eb0f9d9c23d9458c260cfd37797
rbusybee <romiococ@gmail.com>
2020-11-15 22:43:30 +0530

    Added genres express app

---
+++ mongo/validation/mongo-express/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const genres = require('./routes/genres');
+const express = require('express');
+const app = express();
+
+// Database Connection
+mongoose.connect('mongodb://localhost/vidly')
+    .then(()=> console.log('Connected to MongoDB...'))
+    .catch(err => console.log('Could not connected to MongoDB'));
+
+app.use(express.json());
+app.use('/api/genres', genres);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
