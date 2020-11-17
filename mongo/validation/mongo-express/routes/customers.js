818cfa1848dc2fc96f2974bcf0334bb5e2b1e9f0
rbusybee <romiococ@gmail.com>
2020-11-17 21:27:01 +0530

    Added customers api with GET router

---
+++ mongo/validation/mongo-express/routes/customers.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { boolean } = require('joi');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Customer = mongoose.model('Customers', new mongoose.Schema({
+    isGold: {
+        type: Boolean,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
+    },
+    phone: {
+        type: String,
+        required: true,
+        maxlength: 10,
+        minlength: 6
+    }
+}));
+
+router.get('/', async (req,res) => {
+    res.send(await Customer.find().sort('name'))
+});
+
+router.get('/:id',(req,res) => {
+
+});
+
+router.post('/', (req,res) => {
+
+});
+
+router.put('/:id', (req,res) => {
+
+});
+
+router.delete('/:id', (req,res) => {
+
+});
+
+module.exports = router;
