96aa74857bc7d00a98a928dba2aa919dd37b3370
rbusybee <romiococ@gmail.com>
2020-11-18 00:37:27 +0530

    Small fix

--- mongo/validation/mongo-express/routes/customers.js
+++ mongo/validation/mongo-express/routes/customers.js
@@ -17,7 +17,7 @@ router.get('/:id', async (req,res) => {
 });
 
 router.post('/', async (req,res) => {
-    const { error } = validateCustomer(req.body);
+    const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     let customer = new Customer({
@@ -30,7 +30,7 @@ router.post('/', async (req,res) => {
 });
 
 router.put('/:id', async (req,res) => {
-    const { error } = validateCustomer(req.body);
+    const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
     try{
