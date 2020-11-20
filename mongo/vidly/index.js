09c40514c90af2a174a4b8e000a9c8fab07c7e6c
rbusybee <romiococ@gmail.com>
2020-11-20 16:56:06 +0530

    Added mongoDB connection

--- mongo/vidly/index.js
+++ mongo/vidly/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Database Connection
+mongoose.connect('mongodb://localhost/vidly2')
+    .then(()=> console.log('Connected to MongoDB...'))
+    .catch((err) => console.log('Unable to connect MongoDB...',err));
 
 app.use(express.json());
 
-
-const port = process.env.PORT | 8080
+const port = process.env.PORT || 8080;
 app.listen(port,() => console.log(`Listening on port ${port}...`));
