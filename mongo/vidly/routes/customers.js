9f704d70e0f44f25f7e8f1040e089f69d04bc019
rbusybee <romiococ@gmail.com>
2020-11-20 22:12:55 +0530

    Changed post req handler

--- mongo/vidly/routes/customers.js
+++ mongo/vidly/routes/customers.js
@@ -11,7 +11,9 @@ router.post('/', async (req,res) => {
     const { error } = validate(req.body);
     if (error ) return res.status(400).send('Bad request',error.details[0].message);
     const customer = new Customer({
-        name: req.body.name
+        name: req.body.name,
+        phone: req.body.phone,
+        isGold: req.body.isGold
     });
     await customer.save();
     res.send(customer);
