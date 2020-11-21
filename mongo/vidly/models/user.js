0957223a315d239902dd650be679ec88337e78c6
rbusybee <romiococ@gmail.com>
2020-11-21 14:46:50 +0530

    Added User Route, lodash

---
+++ mongo/vidly/models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+// Models: User
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+}), 'users');
+
+// Request Validation
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().required().min(5).max(50),
+        email: Joi.string().required().min(5).max(255).email(),
+        password: Joi.string().required().min(5).max(255)
+    };
+    return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
