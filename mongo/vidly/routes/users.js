c1b785a1d4d213c3c8a05d98ef8c74b46659c365
rbusybee <romiococ@gmail.com>
2020-11-21 15:05:34 +0530

    Added hash password by bcrypt

--- mongo/vidly/routes/users.js
+++ mongo/vidly/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const { User, validate } = require('../models/user');
 const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 router.get('/',async (req,res)=>{
     res.send(await User.find().sort('email'));
@@ -15,7 +16,10 @@ router.post('/', async (req,res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered');
 
+    // Use joi-password-complexity to check password validation
     user = new User(_.pick(req.body, ['name','email','password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
     await user.save();
     res.send(_.pick(user, ['_id','name','email']));
 });
