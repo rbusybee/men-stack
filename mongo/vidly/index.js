880891dbb2ceed77102560f89767fffbafd700d8
rbusybee <romiococ@gmail.com>
2020-11-23 12:03:45 +0530

    Added ENV Config

--- mongo/vidly/index.js
+++ mongo/vidly/index.js
@@ -9,6 +9,12 @@ const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const config = require('config');
+
+if (!config.get('jwtPrivateKey')) {
+    console.log('FATAL ERROR: jwtPrivateKey is not defined');
+    process.exit(1);
+}
 
 // Database Connection
 mongoose.connect('mongodb://localhost/vidly2')
--- mongo/vidly/routes/auth.js
+++ mongo/vidly/routes/auth.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // Auth Route
 router.post('/', async (req,res) => {
@@ -18,7 +19,7 @@ router.post('/', async (req,res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password');
 
-    const token = jwt.sign({_id: user._id}, 'mySecret');
+    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
     res.send(token);
 });
 
