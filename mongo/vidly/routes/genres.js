c69cbb99e20c9c056201ff3eb888d43f9ca2b0e4
rbusybee <romiococ@gmail.com>
2020-11-23 13:36:24 +0530

    Proctect Route via middleware

--- mongo/vidly/routes/genres.js
+++ mongo/vidly/routes/genres.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const { Genre, validate } = require('../models/genre');
+const auth = require('../middleware/auth');
 
 router.get('/',async (req,res)=>{
     res.send(await Genre.find().sort('name'));
 });
 
-router.post('/', async (req,res) => {
+router.post('/', auth, async (req,res) => {
     const { error } = validate(req.body);
     if (error ) return res.status(400).send('Bad request',error.details[0].message);
     const genre = new Genre({
