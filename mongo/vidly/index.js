7c0c79399f74d99cf507a6848af0d161299acbfc
rbusybee <romiococ@gmail.com>
2020-11-24 12:40:38 +0530

    Refactored config module

--- mongo/vidly/index.js
+++ mongo/vidly/index.js
@@ -1,18 +1,12 @@
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const config = require('config');
 const express = require('express');
 const app = express();
 
 require('./init/logging')();
 require('./init/routes')(app);
 require('./init/db')();
-
-// Handlling env variables
-if (!config.get('jwtPrivateKey')) {
-    console.log('FATAL ERROR: jwtPrivateKey is not defined');
-    process.exit(1);
-}
+require('./init/config')();
 
 const port = process.env.PORT || 8080;
 app.listen(port,() => console.log(`Listening on port ${port}...`));
