726dee187f82e60ab0a1d5a7f36f56dbff889f80
rbusybee <romiococ@gmail.com>
2020-11-24 12:11:54 +0530

    Refactored routes

--- mongo/vidly/index.js
+++ mongo/vidly/index.js
@@ -1,19 +1,17 @@
 require('express-async-errors');
-const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const config = require('config');
-const error = require('./middleware/error');
 const winston = require('winston');
 require('winston-mongodb');
+
+const express = require('express');
+const app = express();
+
+require('./init/routes')(app);
+
+
 
 // Error Handeller: Uncaught Exceptions
 winston.handleExceptions(
@@ -46,14 +44,6 @@ mongoose.connect('mongodb://localhost/vidly2')
     .then(()=> console.log('Connected to MongoDB...'))
     .catch((err) => console.log('Unable to connect MongoDB...',err));
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
 
 const port = process.env.PORT || 8080;
 app.listen(port,() => console.log(`Listening on port ${port}...`));
