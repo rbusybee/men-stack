93bca650072be3be17e9e0e78c2def44eb8f3329
rbusybee <romiococ@gmail.com>
2020-11-25 21:36:33 +0530

    Testing a number functions

--- nodejs/testing-demo/tests/lib.test.js
+++ nodejs/testing-demo/tests/lib.test.js
@@ -1,3 +1,16 @@
-test('First test', () => {
-    // throw new Error('Something wrong');
+const lib = require('../lib');
+
+test('absolute - should return +ve when input is +ve', () => {
+    const res = lib.absolute(1);
+    expect(res).toBe(1);
+});
+
+test('absolute - should return +ve when input is -ve', () => {
+    const res = lib.absolute(-1);
+    expect(res).toBe(1);
+});
+
+test('absolute - should return 0 when input is 0', () => {
+    const res = lib.absolute(0);
+    expect(res).toBe(0);
 });
