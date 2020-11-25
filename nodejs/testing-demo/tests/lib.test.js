ad969a9f6e914bbb5b6ff4144cb47565ac8fbb42
rbusybee <romiococ@gmail.com>
2020-11-26 00:42:49 +0530

    Testing String

--- nodejs/testing-demo/tests/lib.test.js
+++ nodejs/testing-demo/tests/lib.test.js
@@ -17,3 +17,13 @@ describe('absolute function', ()=> {
         expect(res).toBe(0);
     });
 });
+
+
+// Testing String: shouldn't be specific, it should be general
+describe('greet', ()=>{
+    it('should return geerting message', ()=>{
+        const res = lib.greet('Romio');
+        expect(res).toMatch(/Romio/);
+        expect(res).toContain('Romio');
+    })
+})
