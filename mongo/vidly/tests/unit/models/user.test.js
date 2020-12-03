63ad64f61321a0f70f52f8ed4a33359aa2d58a84
rbusybee <romiococ@gmail.com>
2020-12-03 20:43:07 +0530

    Added comments

--- mongo/vidly/tests/unit/models/user.test.js
+++ mongo/vidly/tests/unit/models/user.test.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const mongoose = require('mongoose');
 
+// Test Suite
 describe('user.generateAuthToken', ()=>{
     it('should return a valid JWT', ()=>{
         const payload = {
